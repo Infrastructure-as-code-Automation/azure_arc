684fd645a6d7519a3c915a825a00480170c4c3c6
DanteMustCode <52395211+DanteMustCode@users.noreply.github.com>
2025-03-27 11:51:13 +0800

    no AzSHost

--- azure_jumpstart_hcibox/artifacts/PowerShell/New-HCIBoxCluster.ps1
+++ azure_jumpstart_hcibox/artifacts/PowerShell/New-HCIBoxCluster.ps1
@@ -1526,54 +1526,54 @@ function Set-HCIDeployPrereqs {
             Connect-AzAccount -ServicePrincipal -SubscriptionId $subId -TenantId $tenantId -Credential $azureAppCred
             $armtoken = ConvertFrom-SecureStringToPlainText -SecureString ((Get-AzAccessToken -AsSecureString).Token)
 
-            # Workaround for BITS transfer issue
-            Get-NetAdapter StorageA | Disable-NetAdapter -Confirm:$false | Out-Null
-            Get-NetAdapter StorageB | Disable-NetAdapter -Confirm:$false | Out-Null
+            # # Workaround for BITS transfer issue
+            # Get-NetAdapter StorageA | Disable-NetAdapter -Confirm:$false | Out-Null
+            # Get-NetAdapter StorageB | Disable-NetAdapter -Confirm:$false | Out-Null
 
-            #Invoke the registration script.
-            Invoke-AzStackHciArcInitialization -SubscriptionID $subId -ResourceGroup $resourceGroup -TenantID $tenantId -Region $location -Cloud "AzureCloud" -ArmAccessToken $armtoken -AccountID $clientId -ErrorAction Continue
+            # #Invoke the registration script.
+            # Invoke-AzStackHciArcInitialization -SubscriptionID $subId -ResourceGroup $resourceGroup -TenantID $tenantId -Region $location -Cloud "AzureCloud" -ArmAccessToken $armtoken -AccountID $clientId -ErrorAction Continue
 
-            Get-NetAdapter StorageA | Enable-NetAdapter -Confirm:$false | Out-Null
-            Get-NetAdapter StorageB | Enable-NetAdapter -Confirm:$false | Out-Null
+            # Get-NetAdapter StorageA | Enable-NetAdapter -Confirm:$false | Out-Null
+            # Get-NetAdapter StorageB | Enable-NetAdapter -Confirm:$false | Out-Null
         }
     }
 
-    Get-AzConnectedMachine -ResourceGroupName $env:resourceGroup | foreach-object {
+    # Get-AzConnectedMachine -ResourceGroupName $env:resourceGroup | foreach-object {
 
-        Write-Host "Checking extension status for $($PSItem.Name)"
+    #     Write-Host "Checking extension status for $($PSItem.Name)"
 
-        $requiredExtensions = @('AzureEdgeTelemetryAndDiagnostics', 'AzureEdgeDeviceManagement', 'AzureEdgeLifecycleManager')
-        $attempts = 0
-        $maxAttempts = 90
+    #     $requiredExtensions = @('AzureEdgeTelemetryAndDiagnostics', 'AzureEdgeDeviceManagement', 'AzureEdgeLifecycleManager')
+    #     $attempts = 0
+    #     $maxAttempts = 90
 
-        do {
-            $attempts++
-            $extension = Get-AzConnectedMachineExtension -MachineName $PSItem.Name -ResourceGroupName $env:resourceGroup
+    #     do {
+    #         $attempts++
+    #         $extension = Get-AzConnectedMachineExtension -MachineName $PSItem.Name -ResourceGroupName $env:resourceGroup
 
-            foreach ($extensionName in $requiredExtensions) {
-                $extensionTest = $extension | Where-Object { $_.Name -eq $extensionName }
-                if (!$extensionTest) {
-                    Write-Host "$($PSItem.Name) : Extension $extensionName is missing" -ForegroundColor Yellow
-                    $Wait = $true
-                } elseif ($extensionTest.ProvisioningState -ne "Succeeded") {
-                    Write-Host "$($PSItem.Name) : Extension $extensionName is in place, but not yet provisioned. Current state: $($extensionTest.ProvisioningState)" -ForegroundColor Yellow
-                    $Wait = $true
-                } elseif ($extensionTest.ProvisioningState -eq "Succeeded") {
-                    Write-Host "$($PSItem.Name) : Extension $extensionName is in place and provisioned. Current state: $($extensionTest.ProvisioningState)" -ForegroundColor Green
-                    $Wait = $false
-                }
-            }
+    #         foreach ($extensionName in $requiredExtensions) {
+    #             $extensionTest = $extension | Where-Object { $_.Name -eq $extensionName }
+    #             if (!$extensionTest) {
+    #                 Write-Host "$($PSItem.Name) : Extension $extensionName is missing" -ForegroundColor Yellow
+    #                 $Wait = $true
+    #             } elseif ($extensionTest.ProvisioningState -ne "Succeeded") {
+    #                 Write-Host "$($PSItem.Name) : Extension $extensionName is in place, but not yet provisioned. Current state: $($extensionTest.ProvisioningState)" -ForegroundColor Yellow
+    #                 $Wait = $true
+    #             } elseif ($extensionTest.ProvisioningState -eq "Succeeded") {
+    #                 Write-Host "$($PSItem.Name) : Extension $extensionName is in place and provisioned. Current state: $($extensionTest.ProvisioningState)" -ForegroundColor Green
+    #                 $Wait = $false
+    #             }
+    #         }
 
-            if ($Wait){
-            Write-Host "Waiting for extension installation to complete, sleeping for 2 minutes. Attempt $attempts of $maxAttempts"
-            Start-Sleep -Seconds 120
-            } else {
-                break
-            }
+    #         if ($Wait){
+    #         Write-Host "Waiting for extension installation to complete, sleeping for 2 minutes. Attempt $attempts of $maxAttempts"
+    #         Start-Sleep -Seconds 120
+    #         } else {
+    #             break
+    #         }
 
-        } while ($attempts -lt $maxAttempts)
+    #     } while ($attempts -lt $maxAttempts)
 
-       }
+    #    }
 
 }
 
@@ -1893,141 +1893,141 @@ $null = Set-AzResource -ResourceName $env:computername -ResourceGroupName $env:r
 
 Set-HCIDeployPrereqs -HCIBoxConfig $HCIBoxConfig -localCred $localCred -domainCred $domainCred
 
-Set-HCIDeployPrereqsWorkaround
+# Set-HCIDeployPrereqsWorkaround
 
-& "$Env:HCIBoxDir\Generate-ARM-Template.ps1"
+# & "$Env:HCIBoxDir\Generate-ARM-Template.ps1"
 
-#######################################################################################
-# Validate and deploy the cluster
-#######################################################################################
+# #######################################################################################
+# # Validate and deploy the cluster
+# #######################################################################################
 
-Write-Host "[Build cluster - Step 10/11] Validate cluster deployment..." -ForegroundColor Green
+# Write-Host "[Build cluster - Step 10/11] Validate cluster deployment..." -ForegroundColor Green
 
-if ("True" -eq $env:autoDeployClusterResource) {
+# if ("True" -eq $env:autoDeployClusterResource) {
 
-    $DeploymentProgressString = 'Validating Azure Local cluster deployment'
+#     $DeploymentProgressString = 'Validating Azure Local cluster deployment'
 
-    $tags = Get-AzResourceGroup -Name $env:resourceGroup | Select-Object -ExpandProperty Tags
+#     $tags = Get-AzResourceGroup -Name $env:resourceGroup | Select-Object -ExpandProperty Tags
 
-    if ($null -ne $tags) {
-        $tags['DeploymentProgress'] = $DeploymentProgressString
-    } else {
-        $tags = @{'DeploymentProgress' = $DeploymentProgressString }
-    }
+#     if ($null -ne $tags) {
+#         $tags['DeploymentProgress'] = $DeploymentProgressString
+#     } else {
+#         $tags = @{'DeploymentProgress' = $DeploymentProgressString }
+#     }
 
-    $null = Set-AzResourceGroup -ResourceGroupName $env:resourceGroup -Tag $tags
-    $null = Set-AzResource -ResourceName $env:computername -ResourceGroupName $env:resourceGroup -ResourceType 'microsoft.compute/virtualmachines' -Tag $tags -Force
+#     $null = Set-AzResourceGroup -ResourceGroupName $env:resourceGroup -Tag $tags
+#     $null = Set-AzResource -ResourceName $env:computername -ResourceGroupName $env:resourceGroup -ResourceType 'microsoft.compute/virtualmachines' -Tag $tags -Force
 
-$TemplateFile = Join-Path -Path $env:HCIBoxDir -ChildPath "hci.json"
-$TemplateParameterFile = Join-Path -Path $env:HCIBoxDir -ChildPath "hci.parameters.json"
+# $TemplateFile = Join-Path -Path $env:HCIBoxDir -ChildPath "hci.json"
+# $TemplateParameterFile = Join-Path -Path $env:HCIBoxDir -ChildPath "hci.parameters.json"
 
-try {
-    New-AzResourceGroupDeployment -Name 'hcicluster-validate' -ResourceGroupName $env:resourceGroup -TemplateFile $TemplateFile -TemplateParameterFile $TemplateParameterFile -OutVariable ClusterValidationDeployment -ErrorAction Stop
-}
-catch {
-    Write-Output "Validation failed. Re-run New-AzResourceGroupDeployment to retry. Error: $($_.Exception.Message)"
-}
+# try {
+#     New-AzResourceGroupDeployment -Name 'hcicluster-validate' -ResourceGroupName $env:resourceGroup -TemplateFile $TemplateFile -TemplateParameterFile $TemplateParameterFile -OutVariable ClusterValidationDeployment -ErrorAction Stop
+# }
+# catch {
+#     Write-Output "Validation failed. Re-run New-AzResourceGroupDeployment to retry. Error: $($_.Exception.Message)"
+# }
 
 
-<#
-  Adding known governance tags for avoiding disruptions to the deployment. These tags are applicable to ONLY Microsoft-internal Azure lab tenants and designed for managing automated governance processes related to cost optimization and security controls.
-  Some resources are not created by the Bicep template for HCIBox, hence the need to add them here as part of the automation.
-#>
+# <#
+#   Adding known governance tags for avoiding disruptions to the deployment. These tags are applicable to ONLY Microsoft-internal Azure lab tenants and designed for managing automated governance processes related to cost optimization and security controls.
+#   Some resources are not created by the Bicep template for HCIBox, hence the need to add them here as part of the automation.
+# #>
 
-$VmResource = Get-AzResource -ResourceName $env:computername -ResourceGroupName $env:resourceGroup -ResourceType 'microsoft.compute/virtualmachines'
+# $VmResource = Get-AzResource -ResourceName $env:computername -ResourceGroupName $env:resourceGroup -ResourceType 'microsoft.compute/virtualmachines'
 
-if ($VmResource.Tags.ContainsKey('CostControl') -and $VmResource.Tags.ContainsKey('SecurityControl')) {
+# if ($VmResource.Tags.ContainsKey('CostControl') -and $VmResource.Tags.ContainsKey('SecurityControl')) {
 
-    if($VmResource.Tags.CostControl -eq 'Ignore' -and $VmResource.Tags.SecurityControl -eq 'Ignore') {
+#     if($VmResource.Tags.CostControl -eq 'Ignore' -and $VmResource.Tags.SecurityControl -eq 'Ignore') {
 
-        Write-Output "CostControl and SecurityControl tags are set to 'Ignore' for the VM resource, adding them to other resources created by the Azure Local deployment"
+#         Write-Output "CostControl and SecurityControl tags are set to 'Ignore' for the VM resource, adding them to other resources created by the Azure Local deployment"
 
-        $tags = @{
-            'CostControl' = 'Ignore'
-            'SecurityControl' = 'Ignore'
-        }
+#         $tags = @{
+#             'CostControl' = 'Ignore'
+#             'SecurityControl' = 'Ignore'
+#         }
 
-        Get-AzResource -ResourceGroupName $env:resourceGroup -ResourceType 'Microsoft.KeyVault/vaults' | Update-AzTag -Tag $tags -Operation Merge
+#         Get-AzResource -ResourceGroupName $env:resourceGroup -ResourceType 'Microsoft.KeyVault/vaults' | Update-AzTag -Tag $tags -Operation Merge
 
-        Get-AzResource -ResourceGroupName $env:resourceGroup -ResourceType 'Microsoft.Storage/storageAccounts' | Update-AzTag -Tag $tags -Operation Merge
+#         Get-AzResource -ResourceGroupName $env:resourceGroup -ResourceType 'Microsoft.Storage/storageAccounts' | Update-AzTag -Tag $tags -Operation Merge
 
-        Get-AzResource -ResourceGroupName $env:resourceGroup -ResourceType 'Microsoft.Compute/disks' | Update-AzTag -Tag $tags -Operation Merge
+#         Get-AzResource -ResourceGroupName $env:resourceGroup -ResourceType 'Microsoft.Compute/disks' | Update-AzTag -Tag $tags -Operation Merge
 
-    }
+#     }
 
-}
+# }
 
-Write-Host "[Build cluster - Step 11/11] Run cluster deployment..." -ForegroundColor Green
+# Write-Host "[Build cluster - Step 11/11] Run cluster deployment..." -ForegroundColor Green
 
-if ($ClusterValidationDeployment.ProvisioningState -eq "Succeeded") {
+# if ($ClusterValidationDeployment.ProvisioningState -eq "Succeeded") {
 
-    $DeploymentProgressString = 'Deploying Azure Local cluster'
+#     $DeploymentProgressString = 'Deploying Azure Local cluster'
 
-    $tags = Get-AzResourceGroup -Name $env:resourceGroup | Select-Object -ExpandProperty Tags
+#     $tags = Get-AzResourceGroup -Name $env:resourceGroup | Select-Object -ExpandProperty Tags
 
-    if ($null -ne $tags) {
-        $tags['DeploymentProgress'] = $DeploymentProgressString
-    } else {
-        $tags = @{'DeploymentProgress' = $DeploymentProgressString }
-    }
+#     if ($null -ne $tags) {
+#         $tags['DeploymentProgress'] = $DeploymentProgressString
+#     } else {
+#         $tags = @{'DeploymentProgress' = $DeploymentProgressString }
+#     }
 
-    $null = Set-AzResourceGroup -ResourceGroupName $env:resourceGroup -Tag $tags
-    $null = Set-AzResource -ResourceName $env:computername -ResourceGroupName $env:resourceGroup -ResourceType 'microsoft.compute/virtualmachines' -Tag $tags -Force
+#     $null = Set-AzResourceGroup -ResourceGroupName $env:resourceGroup -Tag $tags
+#     $null = Set-AzResource -ResourceName $env:computername -ResourceGroupName $env:resourceGroup -ResourceType 'microsoft.compute/virtualmachines' -Tag $tags -Force
 
-    Write-Host "Validation succeeded. Deploying HCI cluster..."
+#     Write-Host "Validation succeeded. Deploying HCI cluster..."
 
-    try {
-        New-AzResourceGroupDeployment -Name 'hcicluster-deploy' -ResourceGroupName $env:resourceGroup -TemplateFile $TemplateFile -deploymentMode "Deploy" -TemplateParameterFile $TemplateParameterFile -OutVariable ClusterDeployment -ErrorAction Stop
-    }
-    catch {
-        Write-Output "Deployment command failed. Re-run New-AzResourceGroupDeployment to retry. Error: $($_.Exception.Message)"
-    }
+#     try {
+#         New-AzResourceGroupDeployment -Name 'hcicluster-deploy' -ResourceGroupName $env:resourceGroup -TemplateFile $TemplateFile -deploymentMode "Deploy" -TemplateParameterFile $TemplateParameterFile -OutVariable ClusterDeployment -ErrorAction Stop
+#     }
+#     catch {
+#         Write-Output "Deployment command failed. Re-run New-AzResourceGroupDeployment to retry. Error: $($_.Exception.Message)"
+#     }
 
-    if ("True" -eq $env:autoUpgradeClusterResource -and $ClusterDeployment.ProvisioningState -eq "Succeeded") {
+#     if ("True" -eq $env:autoUpgradeClusterResource -and $ClusterDeployment.ProvisioningState -eq "Succeeded") {
 
-        Write-Host "Deployment succeeded. Upgrading HCI cluster..."
+#         Write-Host "Deployment succeeded. Upgrading HCI cluster..."
 
-        $DeploymentProgressString = 'Upgrading Azure Local cluster'
+#         $DeploymentProgressString = 'Upgrading Azure Local cluster'
 
-        $tags = Get-AzResourceGroup -Name $env:resourceGroup | Select-Object -ExpandProperty Tags
+#         $tags = Get-AzResourceGroup -Name $env:resourceGroup | Select-Object -ExpandProperty Tags
 
-        if ($null -ne $tags) {
-            $tags['DeploymentProgress'] = $DeploymentProgressString
-        } else {
-            $tags = @{'DeploymentProgress' = $DeploymentProgressString }
-        }
+#         if ($null -ne $tags) {
+#             $tags['DeploymentProgress'] = $DeploymentProgressString
+#         } else {
+#             $tags = @{'DeploymentProgress' = $DeploymentProgressString }
+#         }
 
-        $null = Set-AzResourceGroup -ResourceGroupName $env:resourceGroup -Tag $tags
-        $null = Set-AzResource -ResourceName $env:computername -ResourceGroupName $env:resourceGroup -ResourceType 'microsoft.compute/virtualmachines' -Tag $tags -Force
+#         $null = Set-AzResourceGroup -ResourceGroupName $env:resourceGroup -Tag $tags
+#         $null = Set-AzResource -ResourceName $env:computername -ResourceGroupName $env:resourceGroup -ResourceType 'microsoft.compute/virtualmachines' -Tag $tags -Force
 
-        Update-HCICluster -HCIBoxConfig $HCIBoxConfig -domainCred $domainCred
+#         Update-HCICluster -HCIBoxConfig $HCIBoxConfig -domainCred $domainCred
 
-    }
-    else {
+#     }
+#     else {
 
-        Write-Host '$autoUpgradeClusterResource is false, skipping HCI cluster upgrade...follow the documentation to upgrade the cluster manually'
+#         Write-Host '$autoUpgradeClusterResource is false, skipping HCI cluster upgrade...follow the documentation to upgrade the cluster manually'
 
-    }
+#     }
 
-}
-else {
+# }
+# else {
 
-    Write-Error "Validation failed. Aborting deployment. Re-run New-AzResourceGroupDeployment to retry."
+#     Write-Error "Validation failed. Aborting deployment. Re-run New-AzResourceGroupDeployment to retry."
 
-}
+# }
 
-}
-else {
-    Write-Host '$autoDeployClusterResource is false, skipping HCI cluster deployment. If desired, follow the documentation to deploy the cluster manually'
-}
+# }
+# else {
+#     Write-Host '$autoDeployClusterResource is false, skipping HCI cluster deployment. If desired, follow the documentation to deploy the cluster manually'
+# }
 
 
 
 $endtime = Get-Date
 $timeSpan = New-TimeSpan -Start $starttime -End $endtime
 Write-Host
-Write-Host "Successfully deployed HCIBox infrastructure." -ForegroundColor Green
-Write-Host "Infrastructure deployment time was $($timeSpan.Hours):$($timeSpan.Minutes) (hh:mm)." -ForegroundColor Green
+Write-Host "debug: Successfully deployed HCIBox infrastructure." -ForegroundColor Green
+Write-Host "debug: Infrastructure deployment time was $($timeSpan.Hours):$($timeSpan.Minutes) (hh:mm)." -ForegroundColor Green
 
 Stop-Transcript
 
